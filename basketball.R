7f26fe485b31c78a427a8a934f94594ba62eafa4
Andy Roy <andy@improvize.com>
2020-01-09 17:09:29 -0600

    added better comments

--- basketball.R
+++ basketball.R
@@ -22,9 +22,17 @@ bb2 <- as.data.table( NBASeasonTeamByYear("UTA","2019") )
 bb2[opponent=='Toronto Raptors',.(tm,opp)]
 bb2[tm>125,.(opponent,tm,opp,x_4)]
 
-# game statistics
+# per game statistics
+## read per game stats and store in a data.table
+## data.tables have some convenient notation for accessing
 bb <- as.data.table( NBAPerGameStatistics(season = 2018) )
-nrow(bb)
-bb[tm=='ATL',1:10]
-bb[,.N,tm]
+
+nrow(bb) # number of rows in the data table
+colnames(bb) # list column names for this table
+
+bb[tm=='ATL',] # return rows where 'tm' equals 'ATL', show all columns
+bb[tm=='ATL',1:10] # return rows where 'tm' equals 'ATL', show only the first 10 columns
+bb[tm=='ATL',.(player,tm,fg,pts)] # return rows where 'tm' equals 'ATL', show specified columns
+
+bb[,.N,tm] # group all rows by 'tm' and show how many per group - .N is a special symbol
 
